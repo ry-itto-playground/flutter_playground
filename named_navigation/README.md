d6a67b5d9a798d4947016f4831a8c78ab28677e5
ryoya ito <horusu.dqx@gmail.com>
2020-04-29 01:39:55 +0900

    りーどみー

--- named_navigation/README.md
+++ named_navigation/README.md
@@ -1,16 +1,3 @@
 # namednavigation
-
-A new Flutter application.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+名前付きナビゲーションサンプル
+MaterialApp のコンストラクタで home を指定すると ‘/‘ 相当のルーティングがされてしまうから、 routes の中で ‘/‘ のルーティングを書くと再定義って言われちゃうっぽい
--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -29,19 +29,13 @@ class Hoge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("first"),
-      ),
+      appBar: AppBar(title: Text("first")),
       body: Center(
         child: RaisedButton(
           child: Text("次の画面を開く"),
           onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Fuga()
-                )
-            );
+                context, MaterialPageRoute(builder: (context) => Fuga()));
           },
         ),
       ),
@@ -53,16 +47,14 @@ class Fuga extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("second"),
-      ),
+      appBar: AppBar(title: Text("second")),
       body: Center(
         child: RaisedButton(
           child: Text("戻る"),
           onPressed: () {
             Navigator.pop(context);
           },
-        ),  
+        ),
       ),
     );
   }
