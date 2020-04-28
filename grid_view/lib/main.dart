5af1779cfec240edab95b0d91e2f818e352a71a5
ryoya ito <horusu.dqx@gmail.com>
2020-04-28 22:58:34 +0900

    grid modify

--- grid_view/lib/main.dart
+++ grid_view/lib/main.dart
@@ -31,13 +31,22 @@ class Hoge extends StatelessWidget {
     "https://avatars3.githubusercontent.com/u/30540303?s=460&v=4",
   ];
 
+//  @override
+//  Widget build(BuildContext context) {
+//    return GridView.extent(
+//      maxCrossAxisExtent: 150,
+//      padding: EdgeInsets.all(4),
+//      mainAxisSpacing: 4,
+//      crossAxisSpacing: 4,
+//      children: images.map((image) => Container(
+//        child: Image.network(image),
+//      )).toList(),
+//    );
+//  }
   @override
   Widget build(BuildContext context) {
-    return GridView.extent(
-      maxCrossAxisExtent: 150,
-      padding: EdgeInsets.all(4),
-      mainAxisSpacing: 4,
-      crossAxisSpacing: 4,
+    return GridView.count(
+      crossAxisCount: 2,
       children: images.map((image) => Container(
         child: Image.network(image),
       )).toList(),
